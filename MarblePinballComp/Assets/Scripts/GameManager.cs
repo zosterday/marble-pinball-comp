f5ef8db1308d1435256b0d779567db8ff0449b0f
zosterday <zosterday@gmail.com>
2024-01-02 18:23:43 -0800

    done, yay😋

--- MarblePinballComp/Assets/Scripts/GameManager.cs
+++ MarblePinballComp/Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private const float WinnerScaleFactor = 120f;
+
     public bool IsSimActive { get; private set; }
 
     public List<Color> Colors
@@ -32,19 +36,38 @@ public class GameManager : MonoBehaviour
 
     private static GameManager instance;
 
-    private float countdownTimer = 60f;
+    private readonly Dictionary<Color, int> scoresByColor = new();
 
-    private Dictionary<Color, int> scoresByColor = new();
+    private readonly Dictionary<Color, GameObject> leaderboardIconsByColor = new();
+
+    private readonly List<GameObject> leaderboardIcons = new();
+
+    private readonly Vector3 screenOffset = new Vector3(70f, 100f, 0f);
+
+    private readonly Vector3 placementYOffset = new Vector3(0f, -50f, 0f);
+
+    private float countdownTimer = 30f;
+
+    private bool isSimEnded = false;
 
     [SerializeField]
-    public List<Color> colors;
+    private List<Color> colors;
 
-    //[SerializeField]
-    //private GameObject endGamePanel;
+    [SerializeField]
+    private GameObject leaderboardIconPrefab;
+
+    [SerializeField]
+    private GameObject endGamePanel;
+
+    [SerializeField]
+    private GameObject leaderboardPanel;
+
+    [SerializeField]
+    private List<TextMeshProUGUI> leaderboardTexts;
 
     [SerializeField]
     private TextMeshProUGUI countdownTimerText;
-
+    
     private void Awake()
     {
         IsSimActive = false;
@@ -56,7 +79,21 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        foreach (var color in colors)
+        {
+            var leaderboardIcon = Instantiate(leaderboardIconPrefab, Vector3.zero, Quaternion.identity);
+
+            leaderboardIcon.transform.SetParent(leaderboardPanel.transform);
+
+            var iconRenderer = leaderboardIcon.GetComponent<Renderer>();
+            iconRenderer.material.color = color;
+
+            leaderboardIcons.Add(leaderboardIcon);
+            leaderboardIconsByColor.Add(color, leaderboardIcon);
+        }
+
         SpawnManager.Instance.StartMarbleSpawn();
+        UpdateLeaderboard();
         IsSimActive = true;
     }
 
@@ -68,9 +105,22 @@ public class GameManager : MonoBehaviour
             return;
         }
 
+        if (isSimEnded)
+        {
+            var marblesRemaining = FindObjectsOfType<Marble>().Length;
+            if (marblesRemaining > 0)
+            {
+                return;
+            }
+
+            IsSimActive = false;
+            DisplayEndGamePanel();
+        }
+
+
+
         if (countdownTimer <= 0f)
         {
-            IsSimActive = false;
             EndGame();
             return;
         }
@@ -95,15 +145,63 @@ public class GameManager : MonoBehaviour
         }
 
         scoresByColor[color] += pointsToAdd;
+
+        UpdateLeaderboard();
+    }
+
+    private void UpdateLeaderboard()
+    {
+        var colorsWithScores = scoresByColor
+            .OrderByDescending(kvp => kvp.Value)
+            .Take(5)
+            .ToArray();
+
+        leaderboardTexts[0].text = $"1st: ";
+        leaderboardTexts[1].text = $"2nd: ";
+        leaderboardTexts[2].text = $"3rd: ";
+        leaderboardTexts[3].text = $"4th: ";
+        leaderboardTexts[4].text = $"5th: ";
+
+        foreach (var iconObj in leaderboardIcons)
+        {
+            iconObj.SetActive(false);
+        }
+
+        for (var i = 0; i < 5; i++)
+        {
+            var icon = leaderboardIconsByColor[colorsWithScores[i].Key];
+            leaderboardTexts[i].text += colorsWithScores[i].Value;
+            SetLeaderboardPosition(icon, i);
+        }
+    }
+
+    private void SetLeaderboardPosition(GameObject leaderboardIcon, int placement)
+    {
+        leaderboardIcon.transform.localPosition = screenOffset + placementYOffset * placement;
+
+        leaderboardIcon.gameObject.SetActive(true);
     }
 
     private void EndGame()
     {
         SpawnManager.Instance.EndMarbleSpawn();
+        isSimEnded = true;
+    }
 
-        //TODO: Make game wait for all the marbles to finish falling. Just stop spawning first
+    private void DisplayEndGamePanel()
+    {
+        var winner = scoresByColor
+            .OrderByDescending(kvp => kvp.Value)
+            .Take(1)
+            .ToArray()[0];
 
-        //call method to determine winner and set correct fields
-        //endGamePanel.SetActive(true);
+        endGamePanel.SetActive(true);
+
+        var leaderboardIcon = Instantiate(leaderboardIconPrefab, Vector3.zero, Quaternion.identity);
+        leaderboardIcon.transform.SetParent(endGamePanel.transform);
+        leaderboardIcon.transform.localScale = new Vector3(WinnerScaleFactor, WinnerScaleFactor);
+
+        var iconRenderer = leaderboardIcon.GetComponent<Renderer>();
+        iconRenderer.material.color = winner.Key;
     }
 }
